8aba5aed6e388ca48ade5acfbedd676276901310
José Gabriel Ferrer Morales <jgabriel.ferrer@gmail.com>
2018-06-03 15:19:22 +0200

    Limpieza de código comentado.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,11 +2,7 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "Hello, world!" example
-    router.get() { req in
-        return "Hello, world!"
-    }
-
+    
     // ------------------- //
     // Comments Controller //
     /* /api/comments       */
@@ -26,15 +22,4 @@ public func routes(_ router: Router) throws {
     let jsonGnomes = json()
     try router.register(collection: jsonGnomes)
 
-    /*
-    let tokenAuthenticationMiddleware = User.tokenAuthMiddleware()
-    let authedRoutes = router.grouped(tokenAuthenticationMiddleware)
-    authedRoutes.get("this/protected/route") { request -> Future<User.PublicUser> in
-        let user = try request.requireAuthenticated(User.self)
-        return try user.authTokens.query(on: request).first().map(to: User.PublicUser.self) { userTokenType in
-            guard let tokenType = userTokenType?.token else { throw Abort.init(HTTPResponseStatus.notFound) }
-            return User.PublicUser(username: user.username, token: tokenType)
-        }
-    }
-    */
 }
